01a5da31653782f0d937675660526e9a8380e6cf
gellison321 <gellison321@gmail.com>
2023-01-23 17:58:45 -0800

    metrics_assignment1

--- stats.py
+++ stats.py
@@ -1,8 +1,13 @@
+import numpy as np 
+import pandas as pd 
+import os
 import statsmodels.api as sm 
-import statsmodels.formula.api as smf 
+import statsmodels.formula.api as smf
+from  statsmodels.stats.weightstats import CompareMeans, DescrStatsW
 
-def linear_regression(X, y):
-    X = sm.add_constant(X)
-    model = sm.OLS(y,X)
-    results = model.fit()
-    return results
+def linear_regression(x, y):
+    return sm.OLS(y.astype(int), np.asarray(sm.add_constant(x.astype(int)))).fit()
+
+
+def test_means(x1, x2):
+    return CompareMeans(DescrStatsW(x1),  DescrStatsW(x2))
